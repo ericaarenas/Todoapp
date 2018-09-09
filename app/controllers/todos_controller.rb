3c00a9caf79a0e7a0d2f95de57267a1fb9a91178
Erica Arenas <erica.arenas.rodriguez@gmail.com>
2018-09-09 07:15:31 -0400

    add edit action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -15,6 +15,22 @@ class TodosController < ApplicationController
   def show 
     @todo = Todo.find(params[:id])
   end
+  
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+  
+  def update 
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      flash[:notice] = "Todo was successfully updated"
+      redirect_to todo_path(@todo)
+    else
+      render 'edit'
+    end     
+  end
+  
+  
   private 
   #Todo lo que ponga a partír de "private" SOLO será usado por este controller
   def todo_params 
